a0acb07444a4c4cc319518104ee0e7867cfe1771
Alan0211 <84466053+Alan0211@users.noreply.github.com>
2021-09-03 12:16:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import { Main } from "./component/Main.jsx";
+
+//render your react application
+ReactDOM.render(<Main />, document.querySelector("#app"));
